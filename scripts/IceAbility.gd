e0e6251ff0bd6c42b72dfaf1aecbad230bc54a09
andrew <androx360@Gmail.com>
2025-08-01 18:47:02 +0100

    Many changes
    - Added tile set
    - Added tiles to background of main scene
    - added mole man
    - changed resolution

---
+++ scripts/IceAbility.gd
@@ -0,0 +1,15 @@
+extends Ability
+class_name IceAbility
+
+@export var projectile_scene: PackedScene
+
+func activate(owner: Node2D):
+	var projectile = projectile_scene.instantiate()
+	projectile.global_position = owner.global_position
+	projectile.direction = Vector2.RIGHT
+	owner.get_parent().add_child(projectile)
+
+
+#This is a specific instance of an ability that then overrides the empty one in ability.
+#This file is put on to a 'resource' which then allows you to set the stats which are available due to being extended at the start
+# of this script.
