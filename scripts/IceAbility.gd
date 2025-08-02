e6c31bb9eee03570ab8743fcd20e68b343161cad
andrew <androx360@Gmail.com>
2025-08-02 13:21:46 +0100

    Ability Updates
    Checking in ice ability

--- scripts/IceAbility.gd
+++ scripts/IceAbility.gd
@@ -1,14 +1,13 @@
 extends Ability
 class_name IceAbility
 
-@export var projectile_scene: PackedScene
+@export var aoe_scene: PackedScene
+@onready var player = get_node("Player")
 
 func activate(owner: Node2D):
-	var projectile = projectile_scene.instantiate()
-	projectile.global_position = owner.global_position
-	projectile.direction = Vector2.RIGHT
-	owner.get_parent().add_child(projectile)
-
+	var aoe = aoe_scene.instantiate()
+	aoe.global_position = owner.global_position
+	owner.get_parent().add_child(aoe)
 
 #This is a specific instance of an ability that then overrides the empty one in ability.
 #This file is put on to a 'resource' which then allows you to set the stats which are available due to being extended at the start
---
+++ scripts/aoe.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var sprite: =$AnimatedSprite2D
+
+func _ready() -> void:
+	call_deferred("aoespell")
+
+func _process(delta):
+	sprite.rotation += 2.0 * delta
+	#owner.get_parent().add_child(self)
+
+func aoespell():
+	print("aoe")
+	var target_scale = Vector2(5, 5)
+	var duration = 2
+	
+	var tween = create_tween()
+	tween.tween_property(sprite,"scale",target_scale, duration).set_trans(tween.TRANS_SINE).set_ease(tween.EASE_OUT)
+	
+	await  tween.finished
+	await get_tree().create_timer(1.0).timeout
+	
+	var shrink_tween = create_tween()
+	shrink_tween.tween_property(sprite,"scale",Vector2(0,0), duration).set_trans(tween.TRANS_SINE).set_ease(tween.EASE_OUT)
+	
