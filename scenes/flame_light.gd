ab884ef64cf90ac4a4a5af006d49d7312ae2b3a3
andrew <androx360@Gmail.com>
2025-08-03 12:32:35 +0100

    Adding LIGHTS

---
+++ scenes/flame_light.gd
@@ -0,0 +1,19 @@
+extends PointLight2D
+
+var light_energy: float
+
+
+func _ready() -> void:
+	tween_flame()
+
+
+
+
+func tween_flame():
+	var tween = create_tween()
+	light_energy = randf_range(0.3, .8)
+	tween.tween_property(self,"energy",light_energy,.4)
+	print(light_energy)
+	await tween.finished
+	await get_tree().create_timer(.5).timeout
+	tween_flame()
