e46c7d2ff28dd92a6ca99397f7543aeed346abab
andrew <androx360@Gmail.com>
2025-08-03 16:47:17 +0100

    - Removing audio from firestorm as it was causing it to error.

--- scripts/FirestormAbility.gd
+++ scripts/FirestormAbility.gd
@@ -7,7 +7,7 @@ class_name FirestormAbility
 func activate(owner: Node2D):
 	if fs_level > 0:
 		var firestorm1 = firestorm_scene.instantiate()
-		AudioController.play_sound(fireball_sound)
+	#	AudioController.play_sound(fireball_sound)
 		firestorm1.global_position = owner.global_position
 		#firestorm1.direction = Vector2.UP
 		owner.get_parent().add_child(firestorm1)
