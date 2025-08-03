7a93739351d887a2821978e5bf366b9a861e36a8
dprings7 <davepringle07@gmail.com>
2025-08-03 17:01:02 +0100

    Add Game Over sound

--- scenes/GameOverScreen.gd
+++ scenes/GameOverScreen.gd
@@ -1,8 +1,9 @@
 extends Control
+@export var game_over_sound = "res://assets/sounds/game_over.wav"
 
 func _ready():
 	$Score.text = "Your final score is: %s" % str(ScoreController.score)
-
+	AudioController.play_sound(game_over_sound, 1)
 
 func _on_restart_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
